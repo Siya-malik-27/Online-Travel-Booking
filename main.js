1b4f4db7b4f6824fab26660af5ae8ed75542f0bd
Ridhi Jain <ridhijain2401@gamil.com>
2025-04-15 11:21:50 +0530

    Fixed navbar menu button. Fixes #4

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const menuBtn = document.getElementById("menubtn");
+const menuBtn = document.getElementById("menu-btn");
 const navLinks = document.getElementById("nav-links");
 const menuBtnIcon = menuBtn.querySelector("i");
 
